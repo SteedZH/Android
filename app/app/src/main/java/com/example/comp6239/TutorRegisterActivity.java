28d7f2fd1d63093f2a1717fab9bcbaf6ea6aafb7
samlai1306 <samlai1306.ost@gmail.com>
2019-04-15 14:12:16 +0100

    created registration avtivity

--- app/app/src/main/java/com/example/comp6239/TutorRegisterActivity.java
+++ app/app/src/main/java/com/example/comp6239/TutorRegisterActivity.java
@@ -1,14 +1,14 @@
 package com.example.comp6239;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class StudentRegisterActivity extends AppCompatActivity {
+public class TutorRegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_register);
+        setContentView(R.layout.activity_tutor_register);
 
         assert getSupportActionBar() != null;   //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
