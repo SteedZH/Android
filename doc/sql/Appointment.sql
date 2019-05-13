860fbc552ef527467e6c0ea2566a2028a46217d8
Steed <steedzh@gmail.com>
2019-05-14 00:12:59 +0100

    add sql doc

---
+++ doc/sql/Appointment.sql
@@ -0,0 +1,69 @@
+/*
+-- Query: SELECT * FROM COMP6239.Appointment
+LIMIT 0, 1000
+
+-- Date: 2019-05-14 00:06
+*/
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (3,3,8,'2019-08-01 00:00:00','2019-08-01 00:01:00',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (4,3,8,'2019-08-01 00:01:00','2019-08-01 00:02:00',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (5,12,11,'2019-08-01 00:01:00','2019-08-01 00:02:00',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (6,12,8,'2019-08-01 00:03:00','2019-08-01 00:04:00',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (7,3,8,'2019-08-01 00:04:00','2019-08-01 00:05:00',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (10,3,8,'2019-08-01 00:10:04','2019-08-01 20:08:20',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (11,3,8,'2019-08-01 02:14:14','2019-08-01 19:07:39',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (12,3,8,'2019-08-01 02:39:32','2019-08-01 22:22:32',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (13,3,8,'2019-08-01 03:38:49','2019-08-01 23:16:58',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (14,3,8,'2019-08-01 01:19:23','2019-08-01 19:44:42',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (15,3,8,'2019-08-01 09:58:46','2019-08-01 17:54:11',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (16,3,8,'2019-08-01 05:12:18','2019-08-01 18:05:10',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (17,3,8,'2019-08-01 00:48:49','2019-08-01 13:16:16',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (18,3,8,'2019-08-01 02:17:44','2019-08-01 23:51:09',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (19,3,8,'2019-08-01 03:52:45','2019-08-01 17:19:00',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (20,3,8,'2019-08-01 00:23:46','2019-08-01 23:09:28',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (21,3,8,'2019-08-01 06:57:56','2019-08-01 23:48:03',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (22,3,8,'2019-08-01 03:59:02','2019-08-01 20:10:34',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (23,3,8,'2019-08-01 10:41:10','2019-08-01 17:49:56',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (24,3,8,'2019-08-01 01:36:54','2019-08-01 14:50:39',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (25,3,8,'2019-08-01 05:41:20','2019-08-01 19:17:25',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (26,3,8,'2019-08-01 09:21:19','2019-08-01 15:05:47',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (27,3,8,'2019-08-01 11:45:21','2019-08-01 23:07:38',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (28,3,8,'2019-08-01 07:08:13','2019-08-01 20:00:20',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (29,3,8,'2019-08-01 07:47:59','2019-08-01 17:48:26',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (30,3,8,'2019-08-01 04:10:44','2019-08-01 20:56:37',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (31,3,8,'2019-08-01 08:56:05','2019-08-01 22:20:05',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (32,3,8,'2019-08-01 10:16:22','2019-08-01 20:05:12',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (33,3,8,'2019-08-01 01:34:47','2019-08-01 12:18:41',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (34,3,8,'2019-08-01 06:57:14','2019-08-01 17:35:42',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (35,3,8,'2019-08-01 05:16:49','2019-08-01 22:55:44',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (36,3,8,'2019-08-01 04:10:27','2019-08-01 18:17:30',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (37,3,8,'2019-08-01 10:00:37','2019-08-01 12:43:18',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (38,3,8,'2019-08-01 11:27:37','2019-08-01 18:56:05',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (110,12,11,'2019-08-01 00:10:04','2019-08-01 20:08:20',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (111,12,11,'2019-08-01 02:14:14','2019-08-01 19:07:39',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (112,12,11,'2019-08-01 02:39:32','2019-08-01 22:22:32',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (113,12,11,'2019-08-01 03:38:49','2019-08-01 23:16:58',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (114,12,11,'2019-08-01 01:19:23','2019-08-01 19:44:42',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (115,12,11,'2019-08-01 09:58:46','2019-08-01 17:54:11',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (116,12,11,'2019-08-01 05:12:18','2019-08-01 18:05:10',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (117,12,11,'2019-08-01 00:48:49','2019-08-01 13:16:16',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (118,12,11,'2019-08-01 02:17:44','2019-08-01 23:51:09',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (119,12,11,'2019-08-01 03:52:45','2019-08-01 17:19:00',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (120,12,11,'2019-08-01 00:23:46','2019-08-01 23:09:28',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (121,12,11,'2019-08-01 06:57:56','2019-08-01 23:48:03',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (122,12,11,'2019-08-01 03:59:02','2019-08-01 20:10:34',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (123,12,11,'2019-08-01 10:41:10','2019-08-01 17:49:56',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (124,12,11,'2019-08-01 01:36:54','2019-08-01 14:50:39',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (125,12,11,'2019-08-01 05:41:20','2019-08-01 19:17:25',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (126,12,11,'2019-08-01 09:21:19','2019-08-01 15:05:47',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (127,12,11,'2019-08-01 11:45:21','2019-08-01 23:07:38',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (128,12,11,'2019-08-01 07:08:13','2019-08-01 20:00:20',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (129,12,11,'2019-08-01 07:47:59','2019-08-01 17:48:26',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (130,12,11,'2019-08-01 04:10:44','2019-08-01 20:56:37',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (131,12,11,'2019-08-01 08:56:05','2019-08-01 22:20:05',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (132,12,11,'2019-08-01 10:16:22','2019-08-01 20:05:12',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (133,12,11,'2019-08-01 01:34:47','2019-08-01 12:18:41',0);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (134,12,11,'2019-08-01 06:57:14','2019-08-01 17:35:42',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (135,12,11,'2019-08-01 05:16:49','2019-08-01 22:55:44',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (136,12,11,'2019-08-01 04:10:27','2019-08-01 18:17:30',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (137,12,11,'2019-08-01 10:00:37','2019-08-01 12:43:18',1);
+INSERT INTO `Appointment` (`appointment_id`,`student_user_id`,`tutor_user_id`,`start_time`,`end_time`,`is_confirm`) VALUES (138,12,11,'2019-08-01 11:27:37','2019-08-01 18:56:05',0);
---
+++ doc/sql/Message.sql
@@ -0,0 +1,53 @@
+/*
+-- Query: SELECT * FROM COMP6239.Message
+LIMIT 0, 1000
+
+-- Date: 2019-05-14 00:06
+*/
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (2,3,8,'2019-05-07 19:53:42','How much',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (3,11,12,'2019-05-09 19:26:29','312312312',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (4,11,12,'2019-05-09 19:26:35','312312312\nfh',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (6,11,8,'2019-05-10 09:37:15','32131',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (8,12,11,'2019-05-12 20:41:13','321321321321',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (9,8,11,'2019-05-12 20:41:22','32131313',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (10,11,8,'2019-05-12 20:43:42','321e231',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (11,3,8,'2019-05-13 23:04:43','Crossing, The',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (12,3,8,'2019-05-13 23:04:43','Ring of Darkness',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (13,3,8,'2019-05-13 23:04:43','Mac and Me',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (14,3,8,'2019-05-13 23:04:43','Ramen Girl, The',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (15,3,8,'2019-05-13 23:04:43','Second Time Around, The',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (16,3,8,'2019-05-13 23:04:43','HealtH',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (17,3,8,'2019-05-13 23:04:43','Barfi!',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (18,3,8,'2019-05-13 23:04:43','Two Men Went to War',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (19,3,8,'2019-05-13 23:04:43','Breakout',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (20,3,8,'2019-05-13 23:04:43','Keane',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (21,8,3,'2019-05-13 23:05:13','Descent',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (22,8,3,'2019-05-13 23:05:13','Lakeboat',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (23,8,3,'2019-05-13 23:05:13','Breaking Point',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (24,8,3,'2019-05-13 23:05:13','How Do You Know',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (25,8,3,'2019-05-13 23:05:13','Fatty and Mabel Adrift',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (26,8,3,'2019-05-13 23:05:13','Dictator, The',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (27,8,3,'2019-05-13 23:05:13','Something to Talk About',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (28,8,3,'2019-05-13 23:05:13','Fraternity Vacation',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (29,8,3,'2019-05-13 23:05:13','First Person Plural',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (30,8,3,'2019-05-13 23:05:13','World, The (Shijie)',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (31,11,12,'2019-05-13 23:05:38','Cruel Intentions 2 (Cruel Intentions 2: Manchester Prep)',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (32,11,12,'2019-05-13 23:05:38','Stir of Echoes',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (33,11,12,'2019-05-13 23:05:38','Dolores Claiborne',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (34,11,12,'2019-05-13 23:05:38','Red Scorpion',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (35,11,12,'2019-05-13 23:05:38','Camp',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (36,11,12,'2019-05-13 23:05:38','Pyramid of Triboulet, The (La pyramide de Triboulet)',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (37,11,12,'2019-05-13 23:05:38','Fallen Angel',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (38,11,12,'2019-05-13 23:05:38','Temptress Moon (Feng Yue)',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (39,11,12,'2019-05-13 23:05:38','Corto Maltese: The Guilded House of Samarkand (La maison dorée de Samarkand)',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (40,11,12,'2019-05-13 23:05:38','New Jersey Drive',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (41,12,11,'2019-05-13 23:05:53','Order, The',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (42,12,11,'2019-05-13 23:05:53','Santa Fe Trail',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (43,12,11,'2019-05-13 23:05:53','Goalie\'s Anxiety at the Penalty Kick, The (Die Angst des Tormanns beim Elfmeter)',1);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (44,12,11,'2019-05-13 23:05:53','Old Dark House, The',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (45,12,11,'2019-05-13 23:05:53','Bo Burnham: Words, Words, Words',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (46,12,11,'2019-05-13 23:05:53','Mirage',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (47,12,11,'2019-05-13 23:05:53','Second Man, The (O Defteros Andras)',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (48,12,11,'2019-05-13 23:05:53','Intersection',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (49,12,11,'2019-05-13 23:05:53','Female Convict Scorpion: Jailhouse 41 (Joshuu sasori: Dai-41 zakkyo-bô)',0);
+INSERT INTO `Message` (`message_id`,`sender_user_id`,`receiver_user_id`,`send_time`,`details`,`is_read`) VALUES (50,12,11,'2019-05-13 23:05:53','Shadows',0);
