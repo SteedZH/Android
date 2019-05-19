d2c5bf4263657384fa8accfcbd2c47a016566d44
samlai1306 <44398107+samlai1306@users.noreply.github.com>
2019-05-19 17:14:52 +0100

    Update DateTimePicker on Registration

--- app/app/src/main/java/com/example/comp6239/StudentRegisterActivity.java
+++ app/app/src/main/java/com/example/comp6239/StudentRegisterActivity.java
@@ -3,20 +3,88 @@ package com.example.comp6239;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+import android.app.TimePickerDialog;
+import android.widget.TimePicker;
+
 
 import com.example.comp6239.utility.AppUser;
 
 public class StudentRegisterActivity extends AppCompatActivity {
+    private int mYear, mMonth, mDay;
+
+    AutoCompleteTextView edtFirstname, edtLastname, edtEmail, edtDOB;
+    EditText edtUsername, edtPw, edtConfirm;
+    Button btnConfirm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_register);
+
+        btnConfirm = (Button)findViewById(R.id.username_sign_in_button) ;
+
+        edtUsername = (EditText)findViewById(R.id.username);
+        edtFirstname = (AutoCompleteTextView)findViewById(R.id.first_name);
+        edtLastname = (AutoCompleteTextView)findViewById(R.id.last_name);
+
+        edtEmail = (AutoCompleteTextView)findViewById(R.id.email);
+        edtPw = (EditText) findViewById(R.id.password);
+        edtConfirm = (EditText) findViewById(R.id.confirm_password);
+
+        edtDOB = (AutoCompleteTextView)findViewById(R.id.dob);
+        edtDOB.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final Calendar c = Calendar.getInstance();
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        edtDOB.setFocusable(true);
+                        edtDOB.requestFocus();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        // Get Current Date
+
+                        mYear = c.get(Calendar.YEAR);
+                        mMonth = c.get(Calendar.MONTH);
+                        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                        DatePickerDialog datePickerDialog = new DatePickerDialog(StudentRegisterActivity.this,
+                                new DatePickerDialog.OnDateSetListener() {
+
+                                    @Override
+                                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                                        edtDOB.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                                    }
+                                }, mYear, mMonth, mDay);
+                        datePickerDialog.show();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+
+            }
+        });
 
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
@@ -56,5 +124,12 @@ public class StudentRegisterActivity extends AppCompatActivity {
                         finish();
                     }})
                 .setNegativeButton(android.R.string.no, null).show();
+    }
+
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        if (view.getId() == R.id.checkBox) {
+            btnConfirm.setEnabled(checked);
+        }
     }
 }
--- app/app/src/main/java/com/example/comp6239/TutorRegisterActivity.java
+++ app/app/src/main/java/com/example/comp6239/TutorRegisterActivity.java
@@ -1,20 +1,67 @@
 package com.example.comp6239;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 public class TutorRegisterActivity extends AppCompatActivity {
+    AutoCompleteTextView edtDOB;
+    private int mYear, mMonth, mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_register);
+
+        edtDOB = (AutoCompleteTextView)findViewById(R.id.dob);
+        edtDOB.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final Calendar c = Calendar.getInstance();
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        edtDOB.setFocusable(true);
+                        edtDOB.requestFocus();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        // Get Current Date
+
+                        mYear = c.get(Calendar.YEAR);
+                        mMonth = c.get(Calendar.MONTH);
+                        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                        DatePickerDialog datePickerDialog = new DatePickerDialog(TutorRegisterActivity.this,
+                                new DatePickerDialog.OnDateSetListener() {
+
+                                    @Override
+                                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                                        edtDOB.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                                    }
+                                }, mYear, mMonth, mDay);
+                        datePickerDialog.show();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+
+            }
+        });
 
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
